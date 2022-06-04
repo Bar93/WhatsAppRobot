68de6d2f08691978313afeeb86e86d79ba336db9
bar <barkeila@gmail.com>
2022-06-04 16:59:57 +0300

    finish

--- src/main/java/Constant.java
+++ src/main/java/Constant.java
@@ -2,7 +2,12 @@ public class Constant {
 
     public static final int WINDOW_WIDTH = 700;
     public static final int WINDOW_HEIGHT = 500;
+    public static final int WINDOW_X = 0;
+    public static final int WINDOW_Y = 0;
+    public static final int LABEL_WIDTH = 200;
+    public static final int LABEL_HEIGHT = 100;
     public static final int BUTTON_WIDTH = 100;
     public static final int BUTTON_HEIGHT = 50;
     public static final String[] CODE_PHONE = { "50", "52", "54", "55","56","57","58"};
+    public static final String FILE_ADDRESS = "C:\\Users\\USER\\Documents\\Whatsapp.txt";
 }
--- src/main/java/WhatsappPanel.java
+++ src/main/java/WhatsappPanel.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class WhatsappPanel extends JPanel {
@@ -18,42 +21,89 @@ public class WhatsappPanel extends JPanel {
     private JTextField messageLabel;
     private JComboBox<String> areaCodePhone;
 
-
-
-
     public WhatsappPanel() {
         this.setLayout(null);
-        this.setBounds(0, 0, Constant.WINDOW_WIDTH, Constant.WINDOW_HEIGHT);
-        this.loginStatus = new JLabel("login status\n");
-        this.loginStatus.setBounds(80,10,200,100);
-        this.loginStatus.setBackground(Color.WHITE);
+        this.setBounds(Constant.WINDOW_X, Constant.WINDOW_Y, Constant.WINDOW_WIDTH, Constant.WINDOW_HEIGHT);
+        this.setBackground(Color.CYAN);
+        this.loginStatus = new JLabel("Login status: Offline");
+        this.loginStatus.setBounds(Constant.WINDOW_X+Constant.WINDOW_WIDTH/70,Constant.WINDOW_Y+Constant.WINDOW_HEIGHT/50,Constant.LABEL_WIDTH,Constant.LABEL_HEIGHT);
+        this.loginStatus.setForeground(Color.WHITE);
         this.add(this.loginStatus);
-        this.messageStatus = new JLabel("message status\n");
-        this.messageStatus.setBounds(300,10,100,100);
-        this.messageStatus.setBackground(Color.WHITE);
+        this.messageStatus = new JLabel("Message status: ");
+        this.messageStatus.setBounds(Constant.WINDOW_X+Constant.WINDOW_WIDTH/70,Constant.WINDOW_Y+Constant.WINDOW_HEIGHT/4,Constant.LABEL_WIDTH,Constant.LABEL_HEIGHT);
+        this.messageStatus.setForeground(Color.WHITE);
         this.add(this.messageStatus);
-        this.newMessage = new JLabel("incoming message:\n");
-        this.newMessage.setBounds(550,10,100,100);
-        this.newMessage.setBackground(Color.WHITE);
+        this.newMessage = new JLabel("Incoming message:");
+        this.newMessage.setBounds(Constant.WINDOW_X+Constant.WINDOW_WIDTH/70,Constant.WINDOW_Y+Constant.WINDOW_HEIGHT/2,Constant.LABEL_WIDTH,Constant.LABEL_HEIGHT);
+        this.newMessage.setForeground(Color.WHITE);
         this.add(this.newMessage);
-        this.phoneLabel = new JTextField("enter phone\n");
-        this.phoneLabel.setBounds(50,150,100,100);
+        this.phoneLabel = new JTextField("Enter phone - seven number only");
+        this.phoneLabel.setBounds(Constant.WINDOW_X+Constant.WINDOW_WIDTH/2+100,Constant.WINDOW_Y+Constant.WINDOW_HEIGHT/2,Constant.LABEL_WIDTH,Constant.LABEL_HEIGHT);
         this.phoneLabel.setBackground(Color.GREEN);
-        this.messageLabel = new JTextField("enter message\n");
-        this.messageLabel.setBounds(160,150,100,100);
-        this.messageLabel.setBackground(Color.GREEN);
         this.add(this.phoneLabel);
+        this.messageLabel = new JTextField("Enter message");
+        this.messageLabel.setBounds(Constant.WINDOW_X+Constant.WINDOW_WIDTH/2,Constant.WINDOW_Y+Constant.WINDOW_HEIGHT/4,Constant.LABEL_WIDTH*2-100,Constant.LABEL_HEIGHT);
+        this.messageLabel.setBackground(Color.GREEN);
         this.add(this.messageLabel);
         this.areaCodePhone = new JComboBox<>(Constant.CODE_PHONE);
-        this.areaCodePhone.setBounds(10,150,30,50);
+        this.areaCodePhone.setBounds(Constant.WINDOW_X+Constant.WINDOW_WIDTH/2,Constant.WINDOW_Y+Constant.WINDOW_HEIGHT/2,Constant.LABEL_WIDTH/2,Constant.LABEL_HEIGHT);
         this.add(areaCodePhone);
-        JButton button = new JButton("submit");
-        button.setBounds(80, 300, 100, 50);
-        this.add(button);
-        button.addActionListener((e) -> {
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(Constant.WINDOW_X+Constant.WINDOW_WIDTH/2,Constant.WINDOW_Y+Constant.WINDOW_HEIGHT/50,Constant.BUTTON_WIDTH,Constant.BUTTON_HEIGHT);
+        this.add(submitButton);
+        submitButton.addActionListener((e) -> {
             getPhoneAndMessage();
+            System.out.println("2");
         });
+        JButton reportButton = new JButton("Report");
+        reportButton.setBounds(Constant.WINDOW_X+Constant.WINDOW_WIDTH/2+200,Constant.WINDOW_Y+Constant.WINDOW_HEIGHT/50,Constant.BUTTON_WIDTH,Constant.BUTTON_HEIGHT);
+        this.add(reportButton);
+        reportButton.addActionListener((e) -> {
+           getReport();
+        });
+        ImageIcon background = new ImageIcon("official-whatsapp-background1.jpg");
+        JLabel backGround = new JLabel(background);
+        backGround.setBounds(Constant.WINDOW_X, Constant.WINDOW_Y, Constant.WINDOW_WIDTH, Constant.WINDOW_HEIGHT);
+        this.add(backGround);
         this.setVisible(true);
+    }
+
+    public void getPhoneAndMessage(){
+        String phone = "972"+this.areaCodePhone.getSelectedItem() + this.phoneLabel.getText();
+        String message = this.messageLabel.getText();
+        if (checkTextPhone(phone) && checkTextOfMessage(message)){
+            login(phone,message);
+        }
+    }
+
+    public boolean checkTextPhone(String phone){
+        boolean ans = true;
+        int index = 0;
+        if (phone.length()!=12) {
+            ans = false;
+            this.messageStatus.setText("Illegal Number");
+        }
+        else {
+            while (index < phone.length()) {
+                if (Character.isLetter(phone.charAt(index))) {
+                    ans = false;
+                    this.messageStatus.setText("Illegal Number");
+                    break;
+                }
+                index++;
+            }
+        }
+        return ans;
+    }
+
+    public boolean checkTextOfMessage(String message){
+        if (message.length()!=0){
+            return true;
+        }
+        else {
+            this.messageStatus.setText("Empty Message");
+            return false;
+        }
     }
 
     public void login (String phone,String message){
@@ -71,7 +121,7 @@ public class WhatsappPanel extends JPanel {
                     e1.printStackTrace();
                 }
                 if (checkLogin!=null) {
-                    this.loginStatus.setText("login");
+                    this.loginStatus.setText("Login status: Online");
                         success=true;
                         checkIfPhoneExist(message);
 
@@ -96,7 +146,7 @@ public class WhatsappPanel extends JPanel {
             }
             while (checkIfPhoneExist!=null) {
                     if (checkIfPhoneExist.contains("invalid")) {
-                        this.messageStatus.setText("invalid number");
+                        this.messageStatus.setText("Message status: Invalid Number ");
                         phoneExist = false;
                         this.driver.close();
                         break;
@@ -116,40 +166,6 @@ public class WhatsappPanel extends JPanel {
 
             }
 
-
-
-    public void getPhoneAndMessage(){
-            String phone = "972"+this.areaCodePhone.getSelectedItem() + this.phoneLabel.getText();
-            String message = this.messageLabel.getText();
-            if (checkTextPhone(phone) && checkTextOfMessage(message)){
-                login(phone,message);
-            }
-        }
-
-    public boolean checkTextPhone(String phone){
-    boolean ans = true;
-    int index = 0;
-    if (phone.length()!=12)
-        ans = false;
-    else {
-        while (index < phone.length()) {
-            if (Character.isLetter(phone.charAt(index))) {
-                ans = false;
-                break;
-            }
-            index++;
-        }
-    }
-    return ans;
-    }
-
-    public boolean checkTextOfMessage(String message){
-        if (message!=null){
-            return true;
-        }
-        else return false;
-    }
-
     public void pastMessage (String message){
         this.driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
             WebElement newMessage = this.driver.findElement(By.xpath("//div[@title=\"Type a message\"]"));
@@ -165,13 +181,13 @@ public class WhatsappPanel extends JPanel {
                 try {
                     List<WebElement> messagesStatus = this.driver.findElements(By.xpath("//div[@class=\"do8e0lj9 l7jjieqr k6y3xtnu\"]"));
                     if (messagesStatus.get(messagesStatus.size()-1).findElement(By.xpath("//span[@aria-label=\" Sent \"]")).isDisplayed()){
-                        this.messageStatus.setText("Sent");
+                        this.messageStatus.setText("Message status: Sent");
                     }
                    if (messagesStatus.get(messagesStatus.size()-1).findElement(By.xpath("//span[@aria-label=\" Delivered \"]")).isDisplayed()){
-                       this.messageStatus.setText("Delivered");
+                       this.messageStatus.setText("Message status: Delivered");
                    }
                     if (messagesStatus.get(messagesStatus.size()-1).findElement(By.xpath("//span[@aria-label=\" Read \"]")).isDisplayed()){
-                        this.messageStatus.setText("Read");
+                        this.messageStatus.setText("Message status: Read");
                     }
                 }
                 catch (Exception e1){
@@ -190,6 +206,7 @@ public class WhatsappPanel extends JPanel {
                     System.out.println("new message");
                     this.newMessage.setText("incoming message: " + lastMessage);
                     newMassage = true;
+                    this.driver.close();
                 }
 
             try {
@@ -200,6 +217,47 @@ public class WhatsappPanel extends JPanel {
         }
     });
         t.start();
+
+    }
+
+    public void getReport(){
+        String report = "";
+        if (this.messageStatus.getText()=="Message status: Invalid Number "){
+            report = "Invalid Number";
+        }
+        else {
+            report = " From: " + "972-"+this.areaCodePhone.getSelectedItem()+"-" + this.phoneLabel.getText() + "\n Message: " + this.messageLabel.getText() + "\n Comment: " + this.newMessage.getText();
+        }
+
+            writeToTextFile(report, Constant.FILE_ADDRESS);
+            System.out.println(readFromTextFile(Constant.FILE_ADDRESS));
+    }
+
+    public static void writeToTextFile(String report,String path){
+        try {
+            FileOutputStream file = new FileOutputStream(path);
+            ObjectOutputStream writer = new ObjectOutputStream(file);
+            writer.writeObject(report);
+            writer.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String readFromTextFile (String path){
+        String report = "";
+        try {
+            FileInputStream file = new FileInputStream(path);
+            ObjectInputStream reader = new ObjectInputStream(file);
+            report = (String) reader.readObject();
+            reader.close();
+        }
+        catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return report;
 
     }
 
